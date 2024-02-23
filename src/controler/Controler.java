ef117af3a9399fb4ecdec09ee0b95e7be8fe7e52
MaxLipAl <135155937+MaxLipAl@users.noreply.github.com>
2024-02-23 22:53:48 +0300

    Update Controler.java

--- src/controler/Controler.java
+++ src/controler/Controler.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class Controler {
     private final DataStudent dataStudent;
@@ -28,6 +28,36 @@ public class Controler {
         scanner = new Scanner(System.in);
     }
 
+    private void assignStudentsToGroups() {
+        // Получаем список всех преподавателей
+        List<Teacher> teachers = dataTeacher.read();
+
+        // Перебираем каждого преподавателя
+        for (Teacher teacher : teachers) {
+            // Получаем предмет, который преподает текущий преподаватель
+            String subject = teacher.getSubject();
+
+            // Получаем список студентов для данного предмета
+            List<Student> studentsForSubject = getStudentsForSubject(subject);
+
+            // Создаем новую учебную группу
+            StudyGroup studyGroup = new StudyGroup(teacher, studentsForSubject);
+
+            // Добавляем учебную группу в сервис
+            studyGroupService.addStudyGroup(studyGroup);
+        }
+    }
+
+    private List<Student> getStudentsForSubject(String subject) {
+        // Получаем список всех студентов
+        List<Student> allStudents = dataStudent.read();
+
+        // Фильтруем студентов, оставляя только тех, кто изучает данный предмет
+        return allStudents.stream()
+                .filter(student -> student.getSubjects().contains(subject))
+                .collect(Collectors.toList());
+    }
+
     public void start() {
         // Создание студентов
         List<String> subjectsIvanov = Arrays.asList("физика", "математика", "русский язык");
@@ -42,23 +72,23 @@ public class Controler {
         List<String> subjectsJohnson = Arrays.asList("физика", "английский язык", "биология");
         Student blinov = new Student(24, "Блинов", 4.2, subjectsJohnson);
 
-        List<String> subjectsWilliams = Arrays.asList("физика", "математика", "английский язык");
-        Student smirnov = new Student(21, "Смирнов", 4.1, subjectsWilliams);
+        List<String> subjectsSmirnov = Arrays.asList("физика", "математика", "английский язык");
+        Student smirnov = new Student(21, "Смирнов", 4.1, subjectsSmirnov);
 
-        List<String> subjectsBrown = Arrays.asList("физика", "английский язык", "русский язык");
-        Student maximov = new Student(20, "Максимов", 4.0, subjectsBrown);
+        List<String> subjectsMaximov = Arrays.asList("физика", "английский язык", "русский язык");
+        Student maximov = new Student(20, "Максимов", 4.0, subjectsMaximov);
 
-        List<String> subjectsJones = Arrays.asList("физика", "английский язык", "математика");
-        Student gagarin = new Student(22, "Гагарин", 4.5, subjectsJones);
+        List<String> subjectsGagarin = Arrays.asList("физика", "английский язык", "математика");
+        Student gagarin = new Student(22, "Гагарин", 4.5, subjectsGagarin);
 
-        List<String> subjectsGarcia = Arrays.asList("физика", "английский язык", "математика");
-        Student kurchatov = new Student(23, "Курчатов", 4.4, subjectsGarcia);
+        List<String> subjectsKurchatov = Arrays.asList("физика", "английский язык", "математика");
+        Student kurchatov = new Student(23, "Курчатов", 4.4, subjectsKurchatov);
 
-        List<String> subjectsMartinez = Arrays.asList("физика", "математика", "русский язык");
-        Student solovev = new Student(24, "Соловьев", 4.3, subjectsMartinez);
+        List<String> subjectsSolovev = Arrays.asList("физика", "математика", "русский язык");
+        Student solovev = new Student(24, "Соловьев", 4.3, subjectsSolovev);
 
-        List<String> subjectsRodriguez = Arrays.asList("физика", "английский язык", "математика");
-        Student rodionov = new Student(25, "Родионов", 4.2, subjectsRodriguez);
+        List<String> subjectsRodionov = Arrays.asList("физика", "английский язык", "математика");
+        Student rodionov = new Student(25, "Родионов", 4.2, subjectsRodionov);
 
         // Добавление студентов в список
         dataStudent.create(ivanov);
@@ -72,6 +102,7 @@ public class Controler {
         dataStudent.create(solovev);
         dataStudent.create(rodionov);
 
+
         // Создание преподавателей
         Teacher physicsTeacher = new Teacher(35, "Петров", "физика");
         Teacher chemistryTeacher = new Teacher(40, "Сидоров", "химия");
@@ -80,28 +111,8 @@ public class Controler {
         dataTeacher.create(physicsTeacher);
         dataTeacher.create(chemistryTeacher);
 
-        // Создание учебных групп
-        List<Student> physicsStudents = new ArrayList<>();
-        physicsStudents.add(ivanov);
-        physicsStudents.add(sidorov);
-        physicsStudents.add(blinov);
-        physicsStudents.add(smirnov);
-        physicsStudents.add(maximov);
-        physicsStudents.add(gagarin);
-        physicsStudents.add(kurchatov);
-        physicsStudents.add(solovev);
-        physicsStudents.add(rodionov);
-             
-        List<Student> chemistryStudents = new ArrayList<>();
-        chemistryStudents.add(petrov);
-
-        // Создание учебных групп для различных предметов
-        StudyGroup physicsGroup = new StudyGroup(physicsTeacher, physicsStudents);
-        StudyGroup chemistryGroup = new StudyGroup(chemistryTeacher, chemistryStudents);
-
-        // Добавление учебных групп в StudyGroupService
-        studyGroupService.addStudyGroup(physicsGroup);
-        studyGroupService.addStudyGroup(chemistryGroup);
+        // Автоматическое добавление студентов в соответствующие группы
+        assignStudentsToGroups();
 
         // Основной цикл программы
         int choice;
@@ -143,7 +154,7 @@ public class Controler {
     private void printTeachers() {
         // Вывод списка преподавателей
         List<Teacher> teachers = dataTeacher.read();
-        teachers.forEach((Consumer<? super Teacher>) teachers);
+        teachers.forEach(teacher -> System.out.println(teacher));
     }
 
     private void printStudyGroupsBySubject() {
